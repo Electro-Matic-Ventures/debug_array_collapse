5ac1c07fc3f267a69fd1d97744c55431e8196026
John Machado <jrmachado@electro-matic.com>
2021-08-16 12:05:51 -0400

    second loop starts at one index higher

--- debug_array_collapse.py
+++ debug_array_collapse.py
@@ -2,12 +2,12 @@ def collapse_array_tia(array, null_value):
     len_array = len(array)
     for i in range(len_array):
         if array[i] == null_value:
-            for j in range(i, len_array):
+            for j in range(i+1, len_array):
                 if array[j] != null_value:
                     array[i], array[j] = array[j], null_value
                     break
 
-my_array = [-1, 2, 4, 1, -1, 5, -1, 7, -1, -1, -1, 10, 1]
+my_array = [-1, 2, 4, 1, -1, 5, -1, 7, -1, -1, -1, 10, -1]
 print(my_array)
 collapse_array_tia(my_array, -1)
 print(my_array)
